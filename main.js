2681e5f857f4217b64fdb5273d722ff5514576b0
abakhsoliani <achi.bakhsoliani@gmail.com>
2021-02-11 00:14:46 +0400

    ivenever finished

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
-rules = [
+var rules = [
     'ეს თამაში ცნობილი "მე არასდროს" შებრუნებული ვარიანტია <br> თუ მოცემულ სიტტუაციაში ყოფილხარ აუცილებლად !სვამ!'
 ]
+
+var ivenever = [
+    'არ დავკარგულვარ სიმთვრალეში',
+'არ დამიძინია გარეთ',
+'არ მიმღერია მთვრალზე კარაოკე',
+'არ ჩამიფსავს მთვრალზე',
+'არ დამიხატავს ტატუ მთვრალზე',
+'არ გამიკეთებია პირსინგი',
+'გამიგზავნია შიშველი სურათები',
+'შევხვედრილვარ ვარსკვლავს',
+'არ ვყოფილვარ ტელევიზორში',
+'არ მომიტეხავს არაფერი',
+'არ მქონია 24 საათიანი უხილობა',
+'არ მომიტყუებია ასაკი',
+'არ დამიმტვრევია მანქანა',
+'არ დავცემულვარ ხალხში',
+'არ მომიპარავს',
+];
+
+
 
 
 $(document).ready(function(){
     $('body').removeClass("not-loaded");
     var current_game = 0;
+
+
+    
+      
+
+    function getRandomIveNever(){
+        if(ivenever.length==0) "<div class='ivenever-card'>ეს იყო ბოლო დავალება<br>ახალი თამაშის ასარჩევად დაბრუნდით <a href='/'>მთავარ გვერდზე</a></div>";
+
+        var randNumber = Math.floor(Math.random() * ivenever.length);
+        var cardValue = ivenever[randNumber];
+
+        ivenever.splice(randNumber, 1);
+        
+        return "<div class='ivenever-card'><div>"+cardValue+"</div><button class='main-button'>შემდეგი პირობა</button></div>";
+
+    }
+
 
     $(".terms .checkbox").click(function(){
         $(".terms .checkbox").toggleClass("active");
@@ -36,5 +73,16 @@ $(document).ready(function(){
 
     $(".main-button.continue").click(function(){
         $(".rules-container").removeClass("active");
-    })
+        $(".game-container").html(getRandomIveNever());
+        setTimeout(function(){ $(".ivenever-card").addClass("active"); }, 600);
+
+        
+
+    });
+
+
+    $(".content").on("click",".ivenever-card .main-button", function(){
+        $(".game-container").html(getRandomIveNever());
+        setTimeout(function(){ $(".ivenever-card").addClass("active"); }, 600);
+      });
 });
