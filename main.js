0ca54bdfcea9551bf150c156cb05ce3fb3c3f1f9
abakhsoliani <achi.bakhsoliani@gmail.com>
2021-02-10 17:47:49 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $('body').removeClass("not-loaded");
+
+
+    $(".terms .checkbox").click(function(){
+        $(".terms .checkbox").toggleClass("active");
+        $("body").toggleClass("terms-done");
+        $(".main-button").toggleClass("disabled");
+
+    });
+
+
+
+    $(".main-button").click(function(){
+        if($(".main-botton").hasClass("disabled")) return;
+        $("body").addClass("select-games");
+        $(".text-content").remove();
+    });
+});
