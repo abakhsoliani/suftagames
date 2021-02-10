479cbec4333ec358dff6b314f7168b548148cb0d
abakhsoliani <achi.bakhsoliani@gmail.com>
2021-02-10 23:40:54 +0400

    rules

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
+rules = [
+    'ეს თამაში ცნობილი "მე არასდროს" შებრუნებული ვარიანტია <br> თუ მოცემულ სიტტუაციაში ყოფილხარ აუცილებლად !სვამ!'
+]
+
 
 $(document).ready(function(){
     $('body').removeClass("not-loaded");
-
+    var current_game = 0;
 
     $(".terms .checkbox").click(function(){
         $(".terms .checkbox").toggleClass("active");
         $("body").toggleClass("terms-done");
-        $(".main-button").toggleClass("disabled");
+        $(".text-content .main-button").toggleClass("disabled");
 
     });
 
 
 
-    $(".main-button").click(function(){
-        if(!$(".main-button").hasClass("disabled")) {
+    $(".text-content .main-button").click(function(){
+        if(!$(".text-content .main-button").hasClass("disabled")) {
             $("body").addClass("select-games");
             $(".text-content").remove();
             setTimeout(function(){ $(".games-container").addClass("active") }, 1000);
         }
 
     });
+
+    $("#ivenever").click(function(){
+        current_game = 0;
+        $(".games-container").removeClass("active");
+        $(".rules").html(rules[current_game]);
+        setTimeout(function(){ $(".rules-container").addClass("active"); }, 400);
+        
+
+    });
+
+    $(".main-button.continue").click(function(){
+        $(".rules-container").removeClass("active");
+    })
 });
